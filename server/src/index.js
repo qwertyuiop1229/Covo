77abb2399f2e2026088ca4c40fc8dddf21d64a0d
qwe <hyuuga1229@gmail.com>
2026-06-07 12:50:03 +0900

    Release v1.0.212

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export default {
