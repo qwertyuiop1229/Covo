74a0f8592fedd2b29d2d525a8fd8b0a3c3e23837
qwe <hyuuga1229@gmail.com>
2026-07-05 19:37:53 +0900

    Chore: delete one-off AI scripts from tools/scripts

--- tools/scripts/extract_modal.py
+++
@@ -1,6 +0,0 @@
-with open('old_index.html', encoding='utf-8') as f:
-    content = f.read()
-start = content.find('<div id="feedbackModal"')
-if start != -1:
-    end = content.find('</body>', start)
-    print(content[start:end])
